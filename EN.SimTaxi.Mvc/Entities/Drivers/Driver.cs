d4d2812310e2fce867c6f7ca1ee9223cdcd16fe0
MibAX <154632432+MibAX@users.noreply.github.com>
2024-07-11 22:56:59 +0300

    Added FullName ONLY get propery to Driver entity

--- EN.SimTaxi.Mvc/Entities/Drivers/Driver.cs
+++ EN.SimTaxi.Mvc/Entities/Drivers/Driver.cs
@@ -1,5 +1,6 @@
 ﻿using EN.SimTaxi.Mvc.Entities.Cars;
 using EN.SimTaxi.Mvc.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EN.SimTaxi.Mvc.Entities.Drivers
 {
@@ -12,5 +13,15 @@ namespace EN.SimTaxi.Mvc.Entities.Drivers
         public Gender Gender { get; set; }
 
         public List<Car> Cars { get; set; } = [];
+
+
+        [NotMapped]
+        public string FullName { 
+            get
+            {
+                return $"{FirstName} {LastName}";
+
+            }
+        }
     }
 }
